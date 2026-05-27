7deb4bbc65535d201f280a2dcbb58ee222fde585
kittycatgirl <anadasilva03@yahoo.com>
2026-05-27 01:21:08 -0300

    fix: adjuste nas variaveis do sequelize

--- backend/bookings-service/src/infrastructure/database/sequelize.js
+++ backend/bookings-service/src/infrastructure/database/sequelize.js
@@ -1,10 +1,15 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('postgres', 'postgres', 'P@ulinha2003', {
-  host: process.env.DB_HOST || 'db', 
-  port: 5432,
-  dialect: 'postgres',
-  logging: console.log 
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME || 'postgres', 
+  process.env.DB_USER || 'postgres', 
+  process.env.DB_PASS || 'P@ulinha2003', 
+  {
+    host: process.env.DB_HOST || 'db', 
+    port: process.env.DB_PORT || 5432,
+    dialect: 'postgres',
+    logging: console.log 
+  }
+);
 
 module.exports = sequelize;
